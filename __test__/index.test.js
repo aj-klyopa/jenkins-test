5bf03d1d3002e8cf88a03b1343b05295b7f68ead
Aleksandr <klyopa@gmail.com>
2022-08-23 11:14:58 +0300

    initial commit

---
+++ __test__/index.test.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+const app = require("/opt/Dev/Jenkins/jenkins-test/index.js");
+
+describe("Get route", () => {
+  it("page should return hello world", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual("Hello world");
+  });
+});
+
+
+afterAll(async () => {
+  await app.close();
+});
