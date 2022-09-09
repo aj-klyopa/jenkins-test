5ba9e273d65ee24c463f333c6ce7f27f0e70b102
Aleksandr <klyopa@gmail.com>
2022-09-09 13:13:43 +0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json("Hello world3");
+  res.status(200).json("Hello world4");
 });
 
 module.exports = app.listen(process.env.PORT || 4000, () =>
